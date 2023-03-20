277f181b01de764d400961287752edf6aa06775a
lihang941 <1329075357@qq.com>
2023-03-20 03:11:15 +0000

    fix: dependencies ethereum-cryptography

---
+++ lib/Constant.d.ts
@@ -0,0 +1,17 @@
+import { KeyPair } from './starkex-lib';
+import { ApiKeyCredentials, ApiTool, Clock } from './apexpro';
+export declare class ClientConfig {
+    apiKeyCredentials: ApiKeyCredentials;
+    starkKeyPair: KeyPair;
+    clock: Clock;
+    apiTool: ApiTool;
+    networkId: number;
+    accountId: string;
+}
+export declare class ENV {
+    url: string;
+    networkId: number;
+    constructor(url: string, networkId: number);
+}
+export declare const PROD: ENV;
+export declare const QA: ENV;
