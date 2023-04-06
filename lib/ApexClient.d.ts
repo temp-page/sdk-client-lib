df1d1d69cbf77915707cb58780d50b28e887e247
lihang941 <1329075357@qq.com>
2023-04-06 04:48:16 +0000

    feat: StarkKey Pre-Check

---
+++ lib/ApexClient.d.ts
@@ -0,0 +1,24 @@
+import { ClientConfig, ENV } from './Constant';
+import { AccountObject, ApiKeyCredentials, ApiTool, CurrencyObject, KeyPair, SymbolInfoObject, UserObject } from './apexpro';
+import { PublicApi } from './PublicApi';
+import { PrivateApi } from './PrivateApi';
+export declare class ApexClient {
+    apiTool: ApiTool;
+    publicApi: PublicApi;
+    privateApi: PrivateApi;
+    clientConfig: ClientConfig;
+    env: ENV;
+    user: UserObject;
+    account: AccountObject;
+    symbols: {
+        [key: string]: SymbolInfoObject;
+    };
+    currency: CurrencyObject[];
+    constructor(env?: ENV);
+    init(apiKeyCredentials: ApiKeyCredentials, startPrivateKey: string | KeyPair, accountId: string): Promise<void>;
+    private initClock;
+    private initConfig;
+    private initSymbol;
+    private checkAccountId;
+    private checkStarkKey;
+}
